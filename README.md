e3766abede913c6c3d73594563f57ff72d0ee3b9
LianderBoi <derderfungames@gmail.com>
2026-09-07 15:25:09 +0800

    added comments

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # selection-structure-practice
-## 
--- practice.py
+++ practice.py
@@ -1,13 +1,12 @@
-#even or odd?
-print("even or odd?")
-x = None
-while x is None:
+# even or odd?
+x = None # sets the value of x to None
+while x is None: # loops until x is not None AND is an integer
     try:
         x = int(input(" enter an integer: "))
     except ValueError:
         print("use a valid integer")
 
-if x % 2 == 0:
+if x % 2 == 0: # checks if x is divisible by 2
     print(f"{x} is even")
 else:
     print(f"{x} is odd")
